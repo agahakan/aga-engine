e5374f73e0cf102efe72a957bb8611c034015433
agahakan <71853052+agahakan@users.noreply.github.com>
2026-03-13 10:12:00 +0100

    add umbrella include

---
+++ engine/aga/include/aga/aga.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <aga/camera/module.hpp>
+#include <aga/core/app.hpp>
+#include <aga/input/module.hpp>
+#include <aga/render/module.hpp>
+#include <aga/transform/module.hpp>
+#include <aga/window/module.hpp>
